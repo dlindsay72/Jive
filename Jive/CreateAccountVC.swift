e44cd370d1d71f0af400ebd0f00c1a1dac669eea
Dan Lindsay <dlindsay72@hotmail.com>
2017-09-19 14:45:33 -0400

    Can now create a user with a username

--- Jive/CreateAccountVC.swift
+++ Jive/CreateAccountVC.swift
@@ -17,6 +17,10 @@ class CreateAccountVC: UIViewController {
     @IBOutlet weak var passwordTextField: UITextField!
     @IBOutlet weak var profileImageView: UIImageView!
     
+    // Properties
+    var avatarName = "profileDefault"
+    var avatarColor = "[0.5, 0.5, 0.5, 1]"
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -39,6 +43,7 @@ class CreateAccountVC: UIViewController {
     }
     
     @IBAction func createAccountBtnPressed(_ sender: Any) {
+        guard let name = usernameTextField.text, usernameTextField.text != nil else { return }
         guard let email = emailTextField.text, emailTextField.text != "" else { return }
         guard let pass = passwordTextField.text, passwordTextField.text != "" else { return }
         
@@ -46,7 +51,13 @@ class CreateAccountVC: UIViewController {
             if success {
                 AuthService.instance.loginUser(email: email, password: pass, completion: { (success) in
                     if success {
-                        print("logged in user!", AuthService.instance.authToken)
+                       // print("logged in user!", AuthService.instance.authToken)
+                        AuthService.instance.createUser(name: name, email: email, avatarName: self.avatarName, avatarColor: self.avatarColor, completion: { (success) in
+                            if success {
+                                print(UserDataService.instance.name, UserDataService.instance.avatarName)
+                                self.performSegue(withIdentifier: UNWIND, sender: nil)
+                            }
+                        })
                     }
                 })
             }
---
+++ Jive/UserDataService.swift
@@ -0,0 +1,32 @@
+//
+//  UserDataService.swift
+//  Jive
+//
+//  Created by Dan Lindsay on 2017-09-19.
+//  Copyright © 2017 Dan Lindsay. All rights reserved.
+//
+
+import Foundation
+
+class UserDataService {
+    
+    static let instance = UserDataService()
+    
+    private(set) public var id = ""
+    private(set) public var avatarColor = ""
+    private(set) public var avatarName = ""
+    private(set) public var email = ""
+    private(set) public var name = ""
+    
+    func setUserData(id: String, color: String, avatarName: String, email: String, name: String) {
+        self.id = id
+        self.avatarColor = color
+        self.avatarName = avatarName
+        self.email = email
+        self.name = name
+    }
+    
+    func setAvatarName(avatarName: String) {
+        self.avatarName = avatarName
+    }
+}
